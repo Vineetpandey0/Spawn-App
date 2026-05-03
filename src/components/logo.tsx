bd5dc9bb1e68e6dbdc32dcd1ea1f9ffc9995455f
Vineet Pandey <vineetpandey0090@gmail.com>
2026-05-03 08:14:52 +0530

    logo added

---
+++ src/components/logo.tsx
@@ -0,0 +1,15 @@
+'use client'
+
+export default function Logo(){
+    return (
+        <div className="flex ">
+            {["#6366f1","#f472b6","#38bdf8","#4ade80"].map((bg, i) => (
+              <div key={i} style={{
+                width: 28, height: 28, borderRadius: "50%",
+                background: bg,
+                marginLeft: i > 0 ? -10 : 0, zIndex: 4 - i,
+              }} />
+            ))}
+        </div>
+    )
+}
